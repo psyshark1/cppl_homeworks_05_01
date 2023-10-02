628de32076eced25484aca1b651780a1016e7af7
Andrey <andrey.zalozhenkov@pikta.ru>
2023-10-02 17:50:29 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+template<typename T>
+T pow2(T v)
+{
+	return v * v;
+}
+
+std::vector<int>& operator * (std::vector<int>& l, std::vector<int>& r)
+{
+	unsigned i{ 0 };
+	for (auto it : r)
+	{
+		r[i] = it * it;
+		++i;
+	}
+	return l;
+}
+std::ostream& operator << (std::ostream& l, std::vector<int>& r)
+{
+	for (auto it : r)
+	{
+		l << it << ' ';
+	}
+	return l;
+}
+
+int main()
+{	
+	std::vector<int> v{ -1, 4, 8 };
+	constexpr unsigned val{ 4 };
+	std::cout << "[IN]: " << val << std::endl;
+	std::cout << "[OUT]: " << pow2(4) << std::endl;
+
+	std::cout << "[IN]: " << v << std::endl;
+	std::cout << "[OUT]: " << pow2(v) << std::endl;
+	return 0;
+}
